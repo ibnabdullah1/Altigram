8a047408d9a7190938a37e830de4823950fca46f
ibnabdullah1 <arfathossen541@gmail.com>
2025-04-29 09:49:09 +0600

    Install Stream video sdk

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -12,6 +12,7 @@ export default function HomeLayout() {
   return (
     <ChatProvider>
       <Stack>
+        <Stack.Screen name="channel" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </ChatProvider>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,23 @@
-// Define global providers
-
 import { Slot } from "expo-router";
+import { useEffect } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AuthProvider from "../providers/AuthProvider";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === "android") {
+        await PermissionsAndroid.requestMultiple([
+          "android.permission.POST_NOTIFICATIONS",
+          "android.permission.BLUETOOTH_CONNECT",
+          "android.permission.RECORD_AUDIO",
+        ]);
+      }
+    };
+    run();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
